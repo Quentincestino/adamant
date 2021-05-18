ac46814bf72cf5d03b3c77e2d3bb37db5a13d764
Quentincestino <quentindutilleul@protonmail.com>
2021-05-18 14:53:28 +0200

    Added the new idt.asm code to the nasm build

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ fn main() {
     nasm_rs::Build::new()
         .target("x86_64-none-none")
         .file("src/arch/x86/gdt.asm")
-        .compile("x86_64_gdt")
-        .expect("Can't compile gdt asm");
+        .file("src/arch/x86/idt.asm")
+        .compile("x86_64_arch")
+        .expect("Can't compile x86_64 asm");
 }
