2a55fd18d090e6ff2ce7634fc012c7f9b854bf91
Quentincestino <quentindutilleul@protonmail.com>
2021-05-18 21:15:18 +0200

    IDT: Ahah imagine you forget to do ret, cringe isn't it ? ^^

--- src/arch/x86/idt.asm
+++ src/arch/x86/idt.asm
@@ -1,11 +1,14 @@
 global enable_interrupts
 enable_interrupts:
     sti
+    ret
 
 global disable_interrupts
 disable_interrupts:
     cli
+    ret
 
 global load_idt
 load_idt:
     lidt [rdi]
+    ret
