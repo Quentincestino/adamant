09517222c7a7eb8411cdd2c615e9eff18182d567
Quentincestino <quentindutilleul@protonmail.com>
2021-05-18 14:59:23 +0200

    Remove unused import

--- src/arch/x86/interrupts.rs
+++ src/arch/x86/interrupts.rs
@@ -1,8 +1,6 @@
-use crate::serial;
-
 use super::idt::*;
 use crate::log::*;
 
-pub extern "x86-interrupt" fn divide_by_zero(isf: InterruptStackFrame) {
+pub extern "x86-interrupt" fn divide_by_zero(_isf: InterruptStackFrame) {
     error("Wait nibba did you rly divide by zero ?", false);
 }
