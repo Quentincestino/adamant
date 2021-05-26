c13698d3e4bc3788824d4769ce6c8d72df607f13
Quentincestino <quentindutilleul@protonmail.com>
2021-05-26 22:39:43 +0200

    Panic: Removed panic handler from main

--- src/main.rs
+++ src/main.rs
@@ -11,25 +11,16 @@ mod serial;
 mod stivale2;
 
 use arch::*;
-use core::panic::PanicInfo;
-use log::*;
 
 #[no_mangle]
 extern "C" fn entry_point(stivale2_struct: usize) -> ! {
     x86::arch_init();
+    ok!("Hello world !");
     stivale2::set_stivale_addr(stivale2_struct);
 
     allocator::init();
-    ok("Welcome to Adamant !");
 
-    loop {}
-}
+    panic!("Panic test");
 
-#[panic_handler]
-fn panic(_infos: &PanicInfo) -> ! {
-    error(
-        "y0 nibba looks like you shitted something if you are here now",
-        false,
-    );
     loop {}
 }
