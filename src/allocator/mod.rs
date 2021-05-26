b8e8746c689d3f1fd996b82b38f1cf86f7c6bfc4
Quentincestino <quentindutilleul@protonmail.com>
2021-05-26 15:43:52 +0200

    Allocator: change on a function rename

--- src/allocator/mod.rs
+++ src/allocator/mod.rs
@@ -1,7 +1,8 @@
 pub mod mem_map;
 pub mod pmm;
 
-use crate::stivale2::get_stivale2_structure;
+use crate::stivale2::stivale_struct;
+
 pub fn init() {
-    mem_map::stivale2_to_memmap(get_stivale2_structure());
+    mem_map::stivale2_to_memmap(stivale_struct());
 }
