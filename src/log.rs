d3d5da894486af5673cf20d273ef850ad88a4624
Quentincestino <quentindutilleul@protonmail.com>
2021-05-26 22:38:18 +0200

    Logger: Added ANSI escape sequences to get color on console

--- src/log.rs
+++ src/log.rs
@@ -1,6 +1,6 @@
 use crate::serial::SerialPort;
 
-use core::fmt::{Arguments, Write};
+use core::fmt::{rt::v1::Argument, Arguments, Write};
 
 pub enum LogLevel {
     Info,
@@ -12,19 +12,55 @@ pub enum LogLevel {
 
 pub static mut LOG_COM: SerialPort = SerialPort::COM1;
 
-pub fn log(level: LogLevel, message: Arguments) {
-    let message_head = match level {
-        LogLevel::Info => "[ INFO ] ",
-        LogLevel::Ok => "[ OK ] ",
-        LogLevel::Warn => "[ WARN ] ",
-        LogLevel::Error => "[ ERROR ] ",
-        LogLevel::Panic => "[ PANIC ] ",
-    };
+pub const ANSI_RESET: &str = "\u{001b}[0m";
+pub const BOLD: &str = "\u{001b}[1m";
 
+pub const INFO_CYAN: &str = "\u{001b}[36m";
+pub const OK_GREEN: &str = "\u{001b}[32m";
+pub const WARN_YELLOW: &str = "\u{001b}[33m";
+pub const ERROR_RED: &str = "\u{001b}[31m";
+pub const PANIC_MAGENTA: &str = "\u{001b}[35m";
+
+pub fn log(level: LogLevel, message: Arguments) {
+    match level {
+        LogLevel::Info => {
+            _print_log(
+                core::format_args!("{}{}[ INFO ]{} ", BOLD, INFO_CYAN, ANSI_RESET),
+                message,
+            );
+        }
+        LogLevel::Ok => {
+            _print_log(
+                core::format_args!("{}{}[ OK ]{} ", BOLD, OK_GREEN, ANSI_RESET),
+                message,
+            );
+        }
+        LogLevel::Warn => {
+            _print_log(
+                core::format_args!("{}{}[ WARN ]{} ", BOLD, WARN_YELLOW, ANSI_RESET),
+                message,
+            );
+        }
+        LogLevel::Error => {
+            _print_log(
+                core::format_args!("{}{}[ ERROR ]{} ", BOLD, ERROR_RED, ANSI_RESET),
+                message,
+            );
+        }
+        LogLevel::Panic => {
+            _print_log(
+                core::format_args!("{}{}[ PANIC ]{} ", BOLD, PANIC_MAGENTA, ANSI_RESET),
+                message,
+            );
+        }
+    }
+}
+
+pub fn _print_log(prefix: Arguments, msg: Arguments) {
     let mut com = unsafe { LOG_COM };
-    let _ = com.write_str(message_head); // We don't care to unwrap
-    let _ = com.write_fmt(message); // Same
-    let _ = com.write_str("\n");
+    com.write_fmt(prefix.clone());
+    com.write_fmt(msg.clone());
+    com.write_str("\n");
 }
 
 #[macro_export]
