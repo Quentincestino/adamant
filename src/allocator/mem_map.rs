18062f11e9327cdfbaa2cc665a231dcffdccdd27
Quentincestino <quentindutilleul@protonmail.com>
2021-05-26 15:38:04 +0200

    Memory map: todo!!!

--- src/allocator/mem_map.rs
+++ src/allocator/mem_map.rs
@@ -7,7 +7,9 @@ pub fn stivale2_to_memmap(stivale2_struct: StivaleStructure) {
     let memmap = stivale2_struct.memory_map();
 
     match memmap {
-        Some(_map) => {}
+        Some(_map) => {
+            todo!()
+        }
         None => {
             error(
                 "No memory map tag found in Stivale2 structure passed to the entry point.",
