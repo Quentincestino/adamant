a407cd5d128a345347c0eebfc62bd9262e2882e9
Quentincestino <quentindutilleul@protonmail.com>
2021-06-02 16:58:47 +0200

    Build: updated file paths after the little refactor of x86_64 arch folder

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 fn main() {
     nasm_rs::Build::new()
         .target("x86_64-none-none")
-        .file("src/arch/x86/gdt.asm")
-        .file("src/arch/x86/idt.asm")
+        .file("src/arch/x86_64/gdt.asm")
+        .file("src/arch/x86_64/idt.asm")
         .compile("x86_64_arch")
         .expect("Can't compile x86_64 asm");
 }
