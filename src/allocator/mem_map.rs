3ed3a8149548661cd7482e1b4bffbed5394a0955
Quentincestino <quentindutilleul@protonmail.com>
2021-06-02 16:59:22 +0200

    Pmm: default value for MemoryArea is now a BadMemory type

--- src/allocator/mem_map.rs
+++ src/allocator/mem_map.rs
@@ -15,7 +15,7 @@ impl MemoryArea {
             start_addr: 0,
             end_addr: 0,
             size: 0,
-            area_type: MemoryAreaType::Usable,
+            area_type: MemoryAreaType::BadMemory,
         }
     }
 
@@ -54,26 +54,6 @@ pub const MAX_ENTRIES: usize = 16; // Since we need to put a constant length to 
 pub type MemoryMap = [MemoryArea; MAX_ENTRIES];
 pub type MemoryMapSubset = [MemoryArea; MAX_ENTRIES];
 
-pub trait MemmapImpl {
-    fn marked_as(&self, area_type: MemoryAreaType) -> (MemoryMapSubset, usize);
-}
-
-impl MemmapImpl for MemoryMap {
-    // Gets all the areas that are marked as `area_type`.
-    // This function returns a subset of the full memory map and the latest index that is not null
-    fn marked_as(&self, area_type: MemoryAreaType) -> (MemoryMapSubset, usize) {
-        let mut subset = [MemoryArea::default(); MAX_ENTRIES];
-        let mut index = 0;
-
-        for area in self {
-            if area.area_type == area_type {
-                subset[index] = *area;
-                index += 1;
-            }
-        }
-        (subset, index)
-    }
-}
 // Gets the memory map, most of cases from a bootloader, to convert it into a bootloader-agnostic one
 pub fn memory_map() -> MemoryMap {
     stivale2_to_memmap(stivale_struct())
